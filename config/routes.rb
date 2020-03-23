2525d9ecc0b728508347b9b3c12db535b4855788
saeki <shingen7303@gmail.com>
2020-03-24 07:43:35 +0900

    Taskコントーラー & Viewファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+
   # ログインまわり
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :tasks
 
   namespace :admin do
     resources :users
