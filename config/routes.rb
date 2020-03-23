67db0029ed53b0cd4350b7cb54f158c4e5a5659e
saeki <shingen7303@gmail.com>
2020-03-23 19:26:03 +0900

    ログイン機能＆ログアウト機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  # ログインまわり
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   namespace :admin do
     resources :users
   end
