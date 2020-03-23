2c3bfe7fef1f81f235f5a299a217251ae67176d5
saeki <shingen7303@gmail.com>
2020-03-23 18:30:02 +0900

    userの編集機能でエラー

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -29,7 +29,7 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to admin_users_path(@user), notice: "ユーザー「#{@user.name}」を更新しました"
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
     else
       render :new
     end
