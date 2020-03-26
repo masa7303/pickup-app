0b27513a47bf469ff54108a648eda09b0b910c76
saeki <shingen7303@gmail.com>
2020-03-26 10:16:33 +0900

    Userモデルと関連付け #3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
 
   has_many :tasks
   has_many :comments
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   has_one_attached :image
+
 end
