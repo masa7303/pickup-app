3e184b8ea3f4a6bd9986d8cba81bf8bb4e21d07a
saeki <shingen7303@gmail.com>
2020-03-28 17:56:50 +0900

    ページネーション表示

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,14 +3,14 @@ class Admin::UsersController < ApplicationController
 
   def index
     @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
+    @users = @q.result(distinct: true).page(params[:page]).reverse_order
   end
 
   def show
     @user = User.find(params[:id])
 
-    @user_followings = @user.followings.page(params[:page]).with_attached_image
-    @user_followers = @user.followers.page(params[:page]).with_attached_image
+    @user_followings = @user.followings.with_attached_image.page(params[:page]).per(6)
+    @user_followers = @user.followers.with_attached_image.page(params[:page]).per(6)
   end
 
   def new
