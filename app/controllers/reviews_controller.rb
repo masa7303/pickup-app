8dec4af33b26b310f10777a28127e28be2f8623e
saeki <shingen7303@gmail.com>
2020-03-30 15:24:16 +0900

    コントローラーとモデル編集 #21

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,52 @@
 class ReviewsController < ApplicationController
   def index
-  end
-
-  def show
-  end
-
-  def new
+    @q = Review.ransack(params[:q])
+    @reviews = @q.result(distinct: true).with_attached_image
   end
 
   def edit
+  end
+
+  def update
+    if @review.update(review_update_params)
+      redirect_to shop_path(@review.shop), success: '口コミを更新しました'
+    else
+      flash.now[:danger] = '口コミを編集できませんでした'
+      render :edit
+    end
+  end
+
+  def create
+    @review = current_user.reviews.build(review_params)
+    if @review.save
+      redirect_to shop_path(params[:shop_id]), success: '口コミ投稿が完了しました'
+    else
+      @shop = Shop.find(params[:shop_id])
+      redirect_to shop_path(@shop), danger: '口コミ投稿が失敗しました'
+    end
+  end
+
+  def destroy
+    @shop = Shop.find(@review.shop_id)
+    @review.destroy!
+    redirect_to shop_path(@shop), success: '口コミを削除しました'
+  end
+
+  private
+
+  def set_review
+    @review = current_user.reviews.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:title, :body, :review_image, :rate).merge(shop_id: params[:shop_id])
+  end
+
+  def review_update_params
+    params.require(:review).permit(:title, :body, :review_image)
+  end
+
+  def search_params
+    params[:q]&.permit(:title, :body)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,12 @@
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :shop
+
+  validates_uniqueness_of :shop_id, scope: :user_id
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :body, length: { maximum: 500 }
+
+  has_one_attached :review_image
+
+
 end
