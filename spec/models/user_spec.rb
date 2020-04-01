fc3298d2c3b77cefabb43a4a77fd3402634913bd
saeki <shingen7303@gmail.com>
2020-04-02 08:15:09 +0900

    Userモデルのテスト追加 #19

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,61 +5,68 @@ RSpec.describe User, type: :model do
 
     describe 'ユーザーの新規登録のバリデーションのテスト' do
 
-      # 正しい時
+      # 正常な場合
       context 'バリデーションが全て正しいとき' do
-        it 'エラーにならない' do
-          user = build(:user)
-          expect(user).to be_valid
+        it 'エラーなし' do
+          @user = User.new(
+            name: "テストユーザー",
+            email: "test@example.com",
+            password: "i-am-test",
+            password_confirmation: "i-am-test",
+            prefecture: "東京都",
+            municipality: "渋谷区"
+          )
+          expect(@user).to be_valid
         end
       end
 
-      # 異常系
-      context '名前が空のとき' do
-        it 'エラーになる' do
-          user = build(:user, name: '')
-          expect(user.valid?).to eq(false)
-          expect(user.errors.messages[:name]).to include('を入力してください')
+      # 異常な場合
+      context '名前がないとき' do
+        it 'エラーあり' do
+          @user = User.new(name: nil)
+          expect(@user.valid?).to eq(false)
+          expect(@user.errors.messages[:name]).to include('を入力してください')
         end
       end
 
-      context 'メールアドレスが空のとき' do
-        it 'エラーになる' do
-          user = build(:user, email: '')
-          expect(user.valid?).to eq(false)
-          expect(user.errors.messages[:email]).to include('を入力してください')
+      context 'メールアドレスがないとき' do
+        it 'エラーあり' do
+          @user = User.new(email: nil)
+          expect(@user.valid?).to eq(false)
+          expect(@user.errors.messages[:email]).to include('を入力してください')
         end
       end
 
       context 'メールアドレスが重複しているとき' do
-        it 'エラーになる' do
-          user = create(:user)
-          duplicated_user = build(:user, email: user.email)
-          expect(duplicated_user.valid?).to eq(false)
-          expect(duplicated_user.errors.messages[:email]).to include('はすでに存在します')
+        it 'エラーあり' do
+          @user = create(:user)
+          @duplicated_user = build(:user, email: @user.email)
+          expect(@duplicated_user.valid?).to eq(false)
+          expect(@duplicated_user.errors.messages[:email]).to include('はすでに存在します')
         end
       end
 
-      context 'パスワードが空のとき' do
-        it 'エラーになる' do
-          user = build(:user, password: '')
-          expect(user.valid?).to eq(false)
-          expect(user.errors.messages[:password]).to include('は6文字以上で入力してください')
-        end
-      end
-
-      context 'パスワードが6文字以下の場合' do
-        it 'エラーになる' do
-          user = build(:user, password: 'passw')
-          expect(user.valid?).to eq(false)
-          expect(user.errors.messages[:password]).to include('は6文字以上で入力してください')
+      context 'パスワードがないとき' do
+        it 'エラーあり' do
+          @user = build(:user, password: '')
+          expect(@user.valid?).to eq(false)
+          expect(@user.errors.messages[:password]).to include('を入力してください')
         end
       end
 
       context 'パスワードが一致しない場合' do
-        it 'エラーになる' do
-          user = build(:user, password: 'password',password_confirmation: '')
-          expect(user.valid?).to eq(false)
-          expect(user.errors.messages[:password_confirmation]).to include('とパスワードの入力が一致しません', 'を入力してください')
+        it 'エラーあり' do
+          @user = build(:user, password: 'password',password_confirmation: '')
+          expect(@user.valid?).to eq(false)
+          expect(@user.errors.messages[:password_confirmation]).to include('とパスワードの入力が一致しません')
+        end
+      end
+
+      context '都道府県がないとき' do
+        it 'エラーあり' do
+          @user = User.new(prefecture: nil)
+          expect(@user.valid?).to eq(false)
+          expect(@user.errors.messages[:name]).to include('を入力してください')
         end
       end
     end
