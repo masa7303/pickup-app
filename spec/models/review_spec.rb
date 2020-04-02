b8638fdb8a7f5cc88550e486f3aa62f44b8f1330
saeki <shingen7303@gmail.com>
2020-04-02 10:27:50 +0900

    Reviewモデルのテスト作成 #19

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
+  describe 'バリデーションのテスト' do
 
-  describe "Factories" do
-    # 有効なファクトリを持つ
-    it "is valid with valid factory" do
-      create(:user)
-      create(:shop)
-      expect(build(:review)).to be_valid
+    describe 'レビューの新規登録にかかるバリデーションのテスト' do
+
+      context "ファクトリが有効なとき" do
+        # 有効なファクトリを持つ
+        it "エラーなし" do
+          create(:user)
+          create(:shop)
+          expect(build(:review)).to be_valid
+        end
+      end
+
+      context "各カラムが不足しているとき" do
+        # タイトルがなければ無効
+        it { is_expected.to validate_presence_of(:title) }
+        # 内容がなければ無効
+        it { is_expected.to validate_presence_of(:body) }
+        # 評価がなければ無効
+        it { is_expected.to validate_presence_of(:rate) }
+      end
+
+      context "関連をもつとき" do
+        it { should belong_to(:user)}
+        it { should belong_to(:shop)}
+      end
+
     end
-  end
 
-  describe "Validations" do
-    # タイトルがなければ無効
-    it { is_expected.to validate_presence_of(:title) }
-    
   end
 end
