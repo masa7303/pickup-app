0c18138c7280bb987e2dd38381e8566a3e9305e8
saeki <shingen7303@gmail.com>
2020-04-03 16:02:58 +0900

    ルーティング追加 #39

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  #メール機能
+  get 'inquiry' => 'inquiry#index'              # 入力画面
+  post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
+
   get 'search', to: 'tasks#search'
 
   get 'shops/search', to: 'shops#search'
--- test/mailers/inquiry_mailer_test.rb
+++ test/mailers/inquiry_mailer_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class InquiryMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def inquiry
+     inquiry = Inquiry.new(name: "侍 太郎", message: "問い合わせメッセージ")
+
+     InquiryMailer.send_mail(inquiry)
+   end
 end
