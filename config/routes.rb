97b6ed8ca6476a9e74e0119cf83626a3f6138789
saeki <shingen7303@gmail.com>
2020-04-03 16:08:01 +0900

    問い合わせフォームのコントローラーとview作成 #39

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   #メール機能
-  get 'inquiry' => 'inquiry#index'              # 入力画面
-  post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
-  post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
+  get 'inquiry', to: 'inquiry#index'              # 入力画面
+  post 'inquiry/confirm', to: 'inquiry#confirm'   # 確認画面
+  post 'inquiry/thanks', to: 'inquiry#thanks'     # 送信完了画面
 
   get 'search', to: 'tasks#search'
 
