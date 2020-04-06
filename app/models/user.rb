dbf2c07b2bb0752d3f8f9e4df1059639e94b912a
saeki <shingen7303@gmail.com>
2020-04-06 15:50:36 +0900

    :+1:imageカラムをcarrierwaveで使えるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :user
 
-  has_one_attached :image
+  mount_uploader :image, ImageUploader
 
   validates :name, presence: true
   validates :prefecture, presence: true
