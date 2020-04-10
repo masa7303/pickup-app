1b12231a76008230af9444a3c75de2257ca2f449
saeki <shingen7303@gmail.com>
2020-04-10 21:08:13 +0900

    :bug:スレッド新規登録画面のレイアウト修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   acts_as_taggable
   include Common
 
+  validates :section, presence: true
   validates :name, presence: true
   validates :description, presence: true
 
